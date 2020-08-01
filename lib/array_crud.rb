2113f3c1f705bf086d3106bc09504539c00d5ce1
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-01 00:32:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+  [] 
 end
 
 def create_an_array
-  
+  ["head", "shoulders", "knees", "toes"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "eyes"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("ears")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
