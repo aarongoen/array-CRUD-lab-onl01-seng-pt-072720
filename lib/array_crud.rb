e820895d74950629ee7a85fa89055995ed35748f
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-01 00:43:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puts create_an_array << "eyes"
+  puts create_an_array = new_array << "eyes"
 end
 
 def add_element_to_start_of_array(array, element)
